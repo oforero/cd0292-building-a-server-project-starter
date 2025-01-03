2e930adc440a4212f8bee209bb0ae2c24c4b3f73
Oscar Mauricio Forero Carrillo <o.forero@icloud.com>
2025-01-03 16:18:55 +0100

    tests: Modify tests to add one test to check the sharp library is not called again if the image is found in the thumbnails cache folder

--- src/tests/resizeImage.spec.ts
+++ src/tests/resizeImage.spec.ts
@@ -10,12 +10,11 @@ describe('resizeImage', () => {
   const width = 200;
   const height = 300;
 
-  beforeEach(() => {
+  it('should resize and save the image when it is not cached', async () => {
     spyOn(fs, 'existsSync').and.callFake((filePath) => {
       // Simulate the file does not exist
-      return (
-        filePath !== path.join(thumbnailsDir, `${width}x${height}-${image}`)
-      );
+      console.info('Simulating file does not exist: ', filePath);
+      return false;
     });
 
     spyOn(fs, 'mkdirSync').and.callFake(() => undefined);
@@ -27,9 +26,7 @@ describe('resizeImage', () => {
         size: 12345,
       }),
     }));
-  });
 
-  it('should resize and save the image when it is not cached', async () => {
     const result = await resizeImage(
       thumbnailsDir,
       imagesDir,
@@ -43,5 +40,36 @@ describe('resizeImage', () => {
       path.join(thumbnailsDir, `${width}x${height}-${image}`)
     );
     expect(sharp.prototype.resize).toHaveBeenCalledWith(width, height); // Verify resize was called
+  });
+
+  it('should return the cached image when it already exists', async () => {
+    spyOn(fs, 'mkdirSync').and.callFake(() => undefined);
+    spyOn(fs, 'existsSync').and.callFake((filePath) => {
+      // Simulate the file does not exist
+      console.info('Simulating file does exist: ', filePath);
+      return true;
+    });
+    spyOn(sharp.prototype, 'resize').and.callFake(() => ({
+      toFile: jasmine.createSpy('toFile').and.resolveTo({
+        format: 'jpeg',
+        width,
+        height,
+        size: 12345,
+      }),
+    }));
+
+    const result = await resizeImage(
+      thumbnailsDir,
+      imagesDir,
+      image,
+      width,
+      height
+    );
+
+    expect(result.fromCache).toBe(true); // Verify it was served from cache
+    expect(result.resizedImagePath).toBe(
+      path.join(thumbnailsDir, `${width}x${height}-${image}`)
+    );
+    expect(sharp.prototype.resize).not.toHaveBeenCalled(); // Ensure resize was not called
   });
 });
